12f812de54967507bdb36fdba21261bede1244fb
Vladislav Radovic <vradovic522@gmail.com>
2024-03-20 13:06:25 +0100

    Add UI

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const date = req.query.date ? new Date(req.query.date) : new Date();
+
+  if (isNaN(date)) {
+    res.status(404);
+    next();
+  } else {
+    const previousDate = new Date(date);
+    previousDate.setDate(previousDate.getDate() - 1);
+    const nextDate = new Date(date);
+    nextDate.setDate(nextDate.getDate() + 1)
+    res.render('index', { date, previousDate, nextDate });
+  }
 });
 
 module.exports = router;
