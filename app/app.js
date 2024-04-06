f8d09dfd13c0c4e0c001cf6d2e8275ac28ad24c0
Vladislav Radovic <vradovic522@gmail.com>
2024-04-06 19:04:14 +0200

    Update css and docker

--- app/app.js
+++ app/app.js
@@ -30,7 +30,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = process.env.ENV === 'development' ? err : {};
+  res.locals.error = process.env.NODE_ENV === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
--- app/routes/index.js
+++ app/routes/index.js
@@ -19,7 +19,6 @@ router.get("/", async function (req, res, next) {
 
     const menu = await getMenu(date);
     res.render("index", { date, previousDate, nextDate, menu });
-    
   }
 });
 
